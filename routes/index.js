4ab85795da2f7989a4cb4bc4db3ef95f0d850a63
Devin Swift <dswift2423@gmail.com>
2020-04-22 16:33:41 -0700

    Only allow index from this route

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,13 @@
 var express = require('express');
+const nm = require('nodemailer')
+const bp = require('body-parser')
 var router = express.Router();
+express().use(bp.urlencoded({extended: true}))
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { page:'Home', menuID:'home' });
-});
-
-router.get('/works', function(req, res, next) {
-    res.render('works');
-})
-
-router.get('/contact', function(req, res, next) {
-    res.render('contact');
 });
 
 module.exports = router;
