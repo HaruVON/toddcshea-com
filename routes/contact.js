8cd260a804e1412db93ae70a1070fdcfcf5ac903
Devin Swift <dswift2423@gmail.com>
2020-04-23 10:40:45 -0700

    Add email capabilities and style

--- routes/contact.js
+++ routes/contact.js
@@ -1,17 +1,31 @@
 var express = require('express');
-var router = express.Router();
 const nm = require('nodemailer')
-const bp = require('body-parser')
-express().use(bp.urlencoded({extended: true}))
 
-router.get('/contact', function(req, res, next) {
-    res.render('contact');
+var router = express.Router();
+
+const GMAIL_USER = proccess.env.GMAIL_USER
+const GMAIL_PASS = process.env.GMAIL_PASS
+
+router.get('/', function(req, res, next) {
+    res.render('contact', {
+		fname: '',
+		email: '',
+		subject: '',
+		message: '',
+		msg: ''});
 });
 
-router.post('/send', (req, res) => {
+router.post('/', (req, res) => {
 
-	const GMAIL_USER = proccess.env.GMAIL_USER
-	const GMAIL_PASS = process.env.GMAIL_PASS
+	var msgS = {
+		msg: 'Email Sent',
+		class: 'black'
+	};
+
+	var msgF = {
+		msg: 'Error: Email failed to send',
+		class: 'red'
+	};
 
 	const smtpTrans = nm.createTransport({
 		host: 'smtp.gmail.com',
@@ -23,7 +37,6 @@ router.post('/send', (req, res) => {
 		}
 	})
 
-	console.log("creating message")
 	const mailOpts = {
 		from: `${req.body.email}`,
 		to: GMAIL_USER,
@@ -34,24 +47,22 @@ router.post('/send', (req, res) => {
 	// Attempt to send the email
 	smtpTrans.sendMail(mailOpts, (error, response) => {
 		if (error) {
-			// catch 404 and forward to error handler
-			app.use(function(req, res, next) {
-				next(createError(404));
-			});
-			
-			// error handler
-			app.use(function(err, req, res, next) {
-				// set locals, only providing error in development
-				res.locals.message = err.message;
-				res.locals.error = req.app.get('env') === 'development' ? err : {};
-			
-				// render the error page
-				res.status(err.status || 500);
-				res.render('error');
-			});
+			console.log('Failed contact form attempt: ')
+			console.log(mailOpts)
+			res.render('contact', {
+				fname: '',
+				email: '',
+				subject: '',
+				message: '',
+				msg: msgF});
 		}
 		else {
-			res.render
+			res.render('contact', {
+				fname: '',
+				email: '',
+				subject: '',
+				message: '',
+				msg: msgS});
 		}
 	})
 })
