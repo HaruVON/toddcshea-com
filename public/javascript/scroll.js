d82836c6ca21e0bc15bfa1fe58a987115d842e74
Devin Swift <dswift2423@gmail.com>
2020-04-25 14:37:55 -0700

    Delete extra comments

--- public/javascript/scroll.js
+++ public/javascript/scroll.js
@@ -16,27 +16,3 @@ $(function() {
         .addTo(controller);
     }
 });
-
-// import "gsap"
-
-// $(function() {
-//     // init
-//     var controller = new ScrollMagic.Controller();
-
-//     // define movement of panels
-//     var wipeAnimation = new TimelineMax()
-//         .fromTo("section.grid-container.strategy-page", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-//         .fromTo("section.grid-container.innovation-page",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-//         .fromTo("section.grid-container.exection-page", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
-
-//     // create scene to pin and link animation
-//     new ScrollMagic.Scene({
-//             triggerElement: "#pinContainer",
-//             triggerHook: "onLeave",
-//             duration: "300%"
-//         })
-//         .setPin("#pinContainer")
-//         .setTween(wipeAnimation)
-//         .addTo(controller);
-
-// });
