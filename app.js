375ab23e650a409fc999cbcddbe44038e98fad64
Devin Swift <bugz@localhost.localdomain>
2020-05-14 13:15:15 -0700

    Update formatting to 'prettier' format

--- app.js
+++ app.js
@@ -1,54 +1,53 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const sslRedirect = require('heroku-ssl-redirect')
-const nm = require('nodemailer');
-const bp = require('body-parser')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sslRedirect = require("heroku-ssl-redirect");
+const nm = require("nodemailer");
+const bp = require("body-parser");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const contactRouter = require('./routes/contact');
-const worksRouter = require('./routes/works');
-const loginRouter = require('./routes/login');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const contactRouter = require("./routes/contact");
+const worksRouter = require("./routes/works");
+const loginRouter = require("./routes/login");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(bp.urlencoded({extended: true}))
-app.use(logger('dev'));
+app.use(bp.urlencoded({ extended: true }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(sslRedirect());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/send', contactRouter);
-app.use('/contact', contactRouter);
-app.use('/works', worksRouter);
-app.use('/login', loginRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/send", contactRouter);
+app.use("/contact", contactRouter);
+app.use("/works", worksRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
